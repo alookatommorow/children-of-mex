57ed0ce449ae75df8c75ae5bc8edf273a36f195e
alookatommorow <hessjohnt@gmail.com>
2016-10-11 19:42:05 -0700

    add first spec

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,3 +1,5 @@
 class Donation < ActiveRecord::Base
-
+  def to_dollars
+    self.amount_in_cents/100
+  end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :donation do
     amount_in_cents 2000
-    email sequence(:email) { |n| "darth_vader#{n}@death.star" }
+    sequence(:email) { |n| "darth_vader#{n}@death.star" }
   end
 end
