c2cead162f50f8b165627958ee87bafa780fc28b
alookatommorow <hessjohnt@gmail.com>
2016-11-14 21:51:40 -0800

    implement donation mailer

--- app/mailers/donation_mailer.rb
+++ app/mailers/donation_mailer.rb
@@ -1,2 +1,8 @@
 class DonationMailer < ApplicationMailer
+  default from: 'childrenofmex@gmail.com'
+
+  def confirmation_email(donation)
+    @donation = donation
+    mail(to: @donation.email, subject: 'Thank you for Your Donation to Children of Mexico International')
+  end
 end
