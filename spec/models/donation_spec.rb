68f7c6137fa64f870e457a15d204883add85d39c
alookatommorow <hessjohnt@gmail.com>
2016-11-14 21:50:19 -0800

    modify to_dollars method

--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -3,9 +3,11 @@ require "rails_helper"
 RSpec.describe Donation do
   describe "#to_dollars" do
     it "returns the donation amount in dollars" do
-      donation = build(:donation)
+      donation = build(:donation, amount_in_cents: 2075)
+      amount_to_dollars = donation.to_dollars
 
-      expect(donation.to_dollars).to eq(20)
+      expect(amount_to_dollars).to eq(20.75)
+      expect(amount_to_dollars).to be_a(Float)
     end
   end
 end
