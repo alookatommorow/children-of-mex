2fd8e380f2c059372c9788eae8499880024819cf
alookatommorow <hessjohnt@gmail.com>
2016-11-16 11:13:24 -0800

    add mailer test cov

--- spec/controllers/donations_controller_spec.rb
+++ spec/controllers/donations_controller_spec.rb
@@ -2,15 +2,22 @@ require "rails_helper"
 
 RSpec.describe DonationsController do
   describe "#create" do
-    it "creates a donation and stripe charge" do
+    it "creates a donation, stripe customer, stripe charge, and sends email" do
       email = "leia@alderaan.net"
-      stub_charge
+      message_delivery = instance_double(ActionMailer::MessageDelivery)
+      allow(DonationMailer).to receive(:confirmation_email).and_return(message_delivery)
+      allow(message_delivery).to receive(:deliver_later)
       stub_create_customer
+      stub_charge
 
-      process :create, method: :post, params: { stripe_token: "THISISATOKEN", donation: {email: email, amount_in_cents: "100", } }
+      process :create, method: :post, params: { stripe_token: "THISISATOKEN", donation: {email: email, amount_in_cents: "100", name: "Princess Leia"} }
 
-      expect(Donation.last.email).to eq(email)
-      expect(Donation.last.amount_in_cents).to eq(100)
+      last_donation = Donation.last
+
+      expect(DonationMailer).to have_received(:confirmation_email).with(last_donation)
+      expect(message_delivery).to have_received(:deliver_later)
+      expect(last_donation.email).to eq(email)
+      expect(last_donation.amount_in_cents).to eq(100)
     end
   end
 end
--- spec/mailers/donation_mailer_spec.rb
+++ spec/mailers/donation_mailer_spec.rb
@@ -1,5 +1,15 @@
 require "rails_helper"
 
-RSpec.describe DonationMailerMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe DonationMailer do
+  describe '#confirmation_email' do
+    it 'sends the confirmation_email' do
+      donation = build(:donation, name: "Lando Calrissian")
+      mail = DonationMailer.confirmation_email(donation)
+
+      expect(mail.subject).to eq("Thank you for Your Donation to Children of Mexico International")
+      expect(mail.to).to eq([donation.email])
+      expect(mail.from).to eq(["childrenofmex@gmail.com"])
+      expect(mail.body.encoded).to include("On behalf of Children of Mexico International")
+    end
+  end
 end
