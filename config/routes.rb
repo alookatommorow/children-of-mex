904802e4f542f6aaa588b0e34228103421f176fe
alookatommorow <hessjohnt@gmail.com>
2016-12-05 18:11:33 -0800

    remove routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  get "about", to: "about#index"
-  get "contact", to: "contact#index"
-  get "faq", to: "faq#index"
-  get "gallery", to: "gallery#index"
-  get "news", to: "news#index"
-  get "testimonials", to: "testimonials#index"
+  #static pages via high_voltage
 
   resources :donations, only: [:new, :create]
-
-  root "home#index"
 end
