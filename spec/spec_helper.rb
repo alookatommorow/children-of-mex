f8069b01979225cbba112ad83c725e8df14a856d
alookatommorow <hessjohnt@gmail.com>
2016-12-06 09:14:59 -0800

    configure spec_helper for cc/circleCI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,11 @@
 # The `.rspec` file also contains a few flags that are not defaults but that
 # users commonly want.
 require 'simplecov'
+# save to CircleCI's artifacts directory if we're on CircleCI
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
 SimpleCov.start
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
